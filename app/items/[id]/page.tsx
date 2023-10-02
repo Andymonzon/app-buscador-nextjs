c7f1755b126fc5497c5243a7c6e2c06f6da67670
andres <valentinomonzonandres@gmail.com>
2023-10-01 22:03:45 -0300

    delete randomUUID

--- app/items/[id]/page.tsx
+++ app/items/[id]/page.tsx
@@ -1,13 +1,12 @@
 import { Suspense } from 'react'
 import { CardInfo } from './components'
 import Skeleton from './skeleton'
-import { randomUUID } from 'crypto'
 
 export function ProductId ({ params }: { params: { id: string } }) {
   return (
     <section>
       <Suspense fallback={<Skeleton />} >
-        <CardInfo id={params.id} key={randomUUID()} />
+        <CardInfo id={params.id} />
       </Suspense>
     </section >
   )
